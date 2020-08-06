7f9227381a7792205b229247ed8153a9bf128ccd
zq.cheng <zq.cheng@goupwith.com>
2020-08-06 17:01:37 +0800

    init

---
+++ src/test/java/com/czq/cas/CasClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.czq.cas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
